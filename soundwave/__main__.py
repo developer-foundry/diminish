9c2fee3285dd9239953e33f2b6fe3b8d011b5096
Eric LaForce <elaforc@gmail.com>
2020-03-15 19:59:45 -0400

    Added ability to record

--- soundwave/__main__.py
+++ soundwave/__main__.py
@@ -5,27 +5,28 @@ import alsaaudio
 
 from soundwave import app
 
-
-def usage():
-    print('usage: playwav.py [-d <device>] <file>', file=sys.stderr)
-    sys.exit(2)
-
-
 if __name__ == '__main__':
-    device = 'default'
+    devicestr = 'default'
+    mode = 0 # 0 = play, 1 = record
 
-    opts, args = getopt.getopt(sys.argv[1:], 'd:')
+    opts, args = getopt.getopt(sys.argv[1:], 'd:r:')
     for o, a in opts:
         if o == '-d':
-            device = a
+            devicestr = a
+        if o == '-r':
+            mode = a
 
     if not args:
-        print('usage: playwav.py [-d <device>] <file>', file=sys.stderr)
+        print('usage: soundwave [-d <device>] [-r <mode>] <file>', file=sys.stderr)
         sys.exit(2)
 
-    f = wave.open(args[0], 'rb')
-    device = alsaaudio.PCM(device=device)
+    device = alsaaudio.PCM(device=devicestr)
 
-    app.play(device, f)
-
-    f.close()
+    if mode == 0:
+        f = wave.open(args[0], 'rb')
+        app.play(device, f)
+        f.close()
+    else:
+        f = open(args[0], 'wb')
+        app.record(devicestr, f)
+        f.close()
--- soundwave/app.py
+++ soundwave/app.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import sys
+import time
 import alsaaudio
 
 
@@ -33,3 +34,33 @@ def play(device, f):
         # Read data from stdin
         device.write(data)
         data = f.readframes(periodsize)
+
+def record(device, f):
+    # Open the device in nonblocking capture mode. The last argument could
+    # just as well have been zero for blocking mode. Then we could have
+    # left out the sleep call in the bottom of the loop
+    inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, device=device)
+
+    # Set attributes: Mono, 44100 Hz, 16 bit little endian samples
+    inp.setchannels(2)
+    inp.setrate(44100)
+    inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+
+    # The period size controls the internal number of frames per period.
+    # The significance of this parameter is documented in the ALSA api.
+    # For our purposes, it is suficcient to know that reads from the device
+    # will return this many frames. Each frame being 2 bytes long.
+    # This means that the reads below will return either 320 bytes of data
+    # or 0 bytes of data. The latter is possible because we are in nonblocking
+    # mode.
+    inp.setperiodsize(160)
+
+    loops = 1000000
+    while loops > 0:
+        loops -= 1
+        # Read data from device
+        l, data = inp.read()
+
+        if l:
+            f.write(data)
+            time.sleep(.001)
