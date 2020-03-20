d4d536a9a571422c5bf6ef1fc586928d955d3b89
Keith LaForce <klaforce@gmail.com>
2020-03-20 10:18:13 -0400

    creating lms and nlms algorithms

--- soundwave/algorithms/least_mean_squares.py
+++ soundwave/algorithms/least_mean_squares.py
@@ -1,2 +1,13 @@
-def least_mean_squares(param):
-  print("Least Mean Squares")
+import padasip as pa
+
+
+def least_mean_squares(inputSignal, targetSignal, mu):
+    f = pa.filters.FilterLMS(n=inputSignal.shape[0], mu=mu, w="random")
+    y, e, w = f.run(targetSignal, inputSignal)
+    return y, e
+
+
+def normalized_least_mean_squares(inputSignal, targetSignal, mu):
+    f = pa.filters.FilterNLMS(n=inputSignal.shape[0], mu=mu, w="random")
+    y, e, w = f.run(targetSignal, inputSignal)
+    return y, e
