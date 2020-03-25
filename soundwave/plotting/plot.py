05d671ac138bc4ef1ec3a2d27cf983fb125970ce
Keith LaForce <klaforce@gmail.com>
2020-03-25 07:06:16 -0400

    adding stack plot

--- soundwave/plotting/plot.py
+++ soundwave/plotting/plot.py
@@ -4,17 +4,33 @@ import os
 import matplotlib.pyplot as plt
 
 
-def plot_simultaneous(algorithm, inputSignal, targetSignal, outputSignal):
+def get_dir(algorithm):
     base_dir = os.getcwd()
     results_dir = os.path.join(base_dir, 'plots/% s/' % (algorithm))
     if not os.path.isdir(results_dir):
         os.makedirs(results_dir)
+    return results_dir
+
+
+def plot_simultaneous(algorithm, inputSignal, targetSignal, outputSignal):
+    algDirectory = get_dir(algorithm)
 
     plt.plot(inputSignal, '-b')
-    plt.savefig(results_dir + 'input.png')
+    plt.savefig(algDirectory + 'input.png')
 
     plt.plot(targetSignal, '-g')
-    plt.savefig(results_dir + 'target.png')
+    plt.savefig(algDirectory + 'target.png')
 
     plt.plot(outputSignal, '-r')
-    plt.savefig(results_dir + 'output.png')
+    plt.savefig(algDirectory + 'output.png')
+
+
+def plot_vertical(algorithm, inputSignal, targetSignal, outputSignal):
+    algDirectory = get_dir(algorithm)
+
+    fig, axs = plt.subplots(3)
+    fig.suptitle('Signals Stacked')
+    axs[0].plot(inputSignal, '-b')
+    axs[1].plot(targetSignal, '-g')
+    axs[2].plot(outputSignal, '-r')
+    plt.savefig(algDirectory + 'allsignals.png')
