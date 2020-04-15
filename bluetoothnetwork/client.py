70be209d5709274bf434539481855ae80856c9e1
Keith La Force <klaforce@gmail.com>
2020-04-15 15:45:21 -0400

    converting to sdp protocol

--- bluetoothnetwork/client.py
+++ bluetoothnetwork/client.py
@@ -3,17 +3,21 @@ import bluetooth
 
 
 def setup(serverAddress):
-    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    port = 0x1001
-    print("Trying to connect to {} on PSM 0x{}...".format(serverAddress, port))
-    sock.connect((serverAddress, port))
-    print("Connected. Type something...")
-    while True:
-        data = input()
-        if not data:
-            break
-        sock.send(data)
-        data = sock.recv(1024)
-        print("Data received:", str(data))
+    uuid = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
+    service_matches = bluetooth.find_service( uuid = uuid )
 
+    if len(service_matches) == 0:
+        print "couldn't find the FooBar service"
+        sys.exit(0)
+
+    first_match = service_matches[0]
+    port = first_match["port"]
+    name = first_match["name"]
+    host = first_match["host"]
+
+    print "connecting to \"%s\" on %s" % (name, host)
+
+    sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+    sock.connect((host, port))
+    sock.send("hello!!")
     sock.close()
--- bluetoothnetwork/server.py
+++ bluetoothnetwork/server.py
@@ -1,26 +1,21 @@
 import bluetooth
 
 def setup():
-    print("Standing up BT server")
-    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    port = 0x1001
-    server_sock.bind(("", port))
-    server_sock.listen(1)
-    #uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ef"
-    #bluetooth.advertise_service(server_sock, "SampleServerL2CAP",
-    #                        service_id=uuid, service_classes = [uuid])
+    server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
-    print("Waiting on connection from client...")
-    client_sock, address = server_sock.accept()
-    print("Accepted connection from", address)
+    port = bluetooth.get_available_port( bluetooth.RFCOMM )
+    server_sock.bind(("",port))
+    server_sock.listen(1)
+    print "listening on port %d" % port
+
+    uuid = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
+    bluetooth.advertise_service( server_sock, "FooBar Service", uuid )
+
+    client_sock,address = server_sock.accept()
+    print "Accepted connection from ",address
 
     data = client_sock.recv(1024)
-    print("Data received:", str(data))
-
-    while data:
-        client_sock.send("Echo =>", str(data))
-        data = client_sock.recv(1024)
-        print("Data received:", str(data))
+    print "received [%s]" % data
 
     client_sock.close()
     server_sock.close()
