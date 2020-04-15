19475eca3345b80171b6b999978979c519b68510
Keith La Force <klaforce@gmail.com>
2020-04-15 12:27:34 -0400

    initial check for bluetooth code organization

---
+++ bluetoothnetwork/__main__.py
@@ -0,0 +1,8 @@
+# simple inquiry example
+import bluetooth as bt
+
+nearby_devices = bt.discover_devices(lookup_names=True)
+print("Found {} devices.".format(len(nearby_devices)))
+
+for addr, name in nearby_devices:
+    print("  {} - {}".format(addr, name))
