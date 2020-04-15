084a169496bb5f6e7a11ce8e96e46a1b9b624919
Keith La Force <klaforce@gmail.com>
2020-04-15 15:49:09 -0400

    upgrading example

--- bluetoothnetwork/client.py
+++ bluetoothnetwork/client.py
@@ -3,11 +3,14 @@ import bluetooth
 
 
 def setup(serverAddress):
-    uuid = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
-    service_matches = bluetooth.find_service( uuid = uuid )
+    addr = None
+
+    # search for the SampleServer service
+    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+    service_matches = bluetooth.find_service(uuid=uuid, address=addr)
 
     if len(service_matches) == 0:
-        print "couldn't find the FooBar service"
+        print("Couldn't find the SampleServer service.")
         sys.exit(0)
 
     first_match = service_matches[0]
@@ -15,9 +18,17 @@ def setup(serverAddress):
     name = first_match["name"]
     host = first_match["host"]
 
-    print "connecting to \"%s\" on %s" % (name, host)
+    print("Connecting to \"{}\" on {}".format(name, host))
 
-    sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+    # Create the client socket
+    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     sock.connect((host, port))
-    sock.send("hello!!")
+
+    print("Connected. Type something...")
+    while True:
+        data = input()
+        if not data:
+            break
+        sock.send(data)
+
     sock.close()
--- bluetoothnetwork/server.py
+++ bluetoothnetwork/server.py
@@ -1,21 +1,36 @@
 import bluetooth
 
 def setup():
-    server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-
-    port = bluetooth.get_available_port( bluetooth.RFCOMM )
-    server_sock.bind(("",port))
+    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    server_sock.bind(("", bluetooth.PORT_ANY))
     server_sock.listen(1)
-    print "listening on port %d" % port
 
-    uuid = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
-    bluetooth.advertise_service( server_sock, "FooBar Service", uuid )
+    port = server_sock.getsockname()[1]
 
-    client_sock,address = server_sock.accept()
-    print "Accepted connection from ",address
+    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-    data = client_sock.recv(1024)
-    print "received [%s]" % data
+    bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
+                                service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                                profiles=[bluetooth.SERIAL_PORT_PROFILE],
+                                # protocols=[bluetooth.OBEX_UUID]
+                                )
+
+    print("Waiting for connection on RFCOMM channel", port)
+
+    client_sock, client_info = server_sock.accept()
+    print("Accepted connection from", client_info)
+
+    try:
+        while True:
+            data = client_sock.recv(1024)
+            if not data:
+                break
+            print("Received", data)
+    except OSError:
+        pass
+
+    print("Disconnected.")
 
     client_sock.close()
     server_sock.close()
+    print("All done.")
