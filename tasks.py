e626a47a45de07b842ed265ca432a8b328ee9815
Eric LaForce <elaforc@gmail.com>
2020-04-18 15:14:26 -0400

    Updated compiler options for PC

--- tasks.py
+++ tasks.py
@@ -1,16 +1,29 @@
 from invoke import task
 
 @task
-def build(c):
+def build_pi_library(c):
   print("Building")
   c.run("gcc -c -Wall -Werror -O3 -funsafe-math-optimizations -fpic -mcpu=cortex-a53 -mfpu=neon-fp-armv8 -mfloat-abi=hard filtering.c")
   c.run("gcc -shared -o filtering.so filtering.o")
   print("Completed Build")
 
 @task
-def build_exec(c):
+def build_exec_pi(c):
   print("Building")
   c.run("gcc -Wall -Werror -O3 -funsafe-math-optimizations -fpic -mcpu=cortex-a53 -mfpu=neon-fp-armv8 -mfloat-abi=hard filtering.c")
+  print("Completed Build")
+
+@task
+def build_pc_library(c):
+  print("Building")
+  c.run("gcc -c -Wall -Werror -O3 -funsafe-math-optimizations -fpic filtering.c")
+  c.run("gcc -shared -o filtering.so filtering.o")
+  print("Completed Build")
+
+@task
+def build_exec_pc(c):
+  print("Building")
+  c.run("gcc -Wall -Werror -O3 -funsafe-math-optimizations -fpic filtering.c")
   print("Completed Build")
 
 @task
