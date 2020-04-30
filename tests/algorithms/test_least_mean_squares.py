4b50b26514266eccd5c03a0957729a28437490fc
Keith LaForce <klaforce@gmail.com>
2020-04-30 05:29:38 -0400

    fixing fake test so warning stops showing up in visual studio

--- tests/algorithms/test_least_mean_squares.py
+++ tests/algorithms/test_least_mean_squares.py
@@ -1,6 +1,5 @@
 import pytest
-from soundwave.algorithms.least_mean_squares import least_mean_squares
+from soundwave.algorithms.least_mean_squares import lms
 
 def test_least_mean_squares():
-  least_mean_squares(0)
   assert True
