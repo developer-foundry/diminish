4d07a689dd5a50cbd5b0452bdf1b36c8dc160664
Keith LaForce <klaforce@gmail.com>
2020-05-07 10:15:49 -0400

    tweak

--- tui/dashboard/dashboardForm.py
+++ tui/dashboard/dashboardForm.py
@@ -4,7 +4,7 @@ from tui.palette.palette import palette
 class DashboardForm():
     def __init__(self, parameters):
         self.parameters = parameters
-        
+
         header_text = urwid.Text(u' Stock Quotes')
         header = urwid.AttrMap(header_text, 'banner')
 
@@ -19,7 +19,12 @@ class DashboardForm():
         quote_box = urwid.LineBox(v_padding)
 
         layout = urwid.Frame(header=header, body=quote_box, footer=menu)
-        self.loop = urwid.MainLoop(layout, palette=palette)
+        self.loop = urwid.MainLoop(layout, palette=palette, unhandled_input=self.handle_input)
     
     def run(self):
         self.loop.run()
+    
+    # Handle key presses
+    def handle_input(self, key):
+        if key == 'Q' or key == 'q':
+            raise urwid.ExitMainLoop()
