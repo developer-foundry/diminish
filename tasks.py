28d7cfa486e20726bcbb8ab1328f980ee82e3343
Eric LaForce <elaforc@gmail.com>
2020-05-20 06:45:45 -0400

    Fixes #24 and #25 - clean up C libraries

--- tasks.py
+++ tasks.py
@@ -1,39 +1,17 @@
 from invoke import task
 
 @task
-def build_pi_library(c):
+def build_libraries(c):
   print("Building")
-  c.run("gcc -c -Wall -Werror -O3 -funsafe-math-optimizations -fpic -mcpu=cortex-a53 -mfpu=neon-fp-armv8 -mfloat-abi=hard filtering.c")
-  c.run("gcc -shared -o filtering.so filtering.o")
-  print("Completed Build")
-
-@task
-def build_exec_pi(c):
-  print("Building")
-  c.run("gcc -Wall -Werror -O3 -funsafe-math-optimizations -fpic -mcpu=cortex-a53 -mfpu=neon-fp-armv8 -mfloat-abi=hard filtering.c")
-  print("Completed Build")
-
-@task
-def build_pc_library(c):
-  print("Building")
-  c.run("gcc -c -Wall -Werror -O3 -funsafe-math-optimizations -fpic filtering.c")
-  c.run("gcc -shared -o filtering.so filtering.o")
-  print("Completed Build")
-
-@task
-def build_exec_pc(c):
-  print("Building")
-  c.run("gcc -Wall -Werror -O3 -funsafe-math-optimizations -fpic filtering.c")
-  print("Completed Build")
-
-@task
-def build_networking(c):
-  print("Building")
-  c.run("gcc -c -Wall -Werror -O3 -fPIC soundwave/networking/server.c soundwave/networking/dotenv.c")
+  c.run("gcc -c -Wall -Werror -O3 -fPIC external-libraries/server.c external-libraries/dotenv.c")
   c.run("gcc -shared -o server.so server.o dotenv.o")
 
-  c.run("gcc -c -Wall -Werror -O3 -fPIC soundwave/networking/client.c soundwave/networking/dotenv.c")
+  c.run("gcc -c -Wall -Werror -O3 -fPIC external-libraries/client.c external-libraries/dotenv.c")
   c.run("gcc -shared -o client.so client.o dotenv.o")
+
+  c.run("gcc -c -Wall -Werror -O3 -funsafe-math-optimizations -fpic external-libraries/filtering.c")
+  c.run("gcc -shared -o filtering.so filtering.o")
+
   print("Completed Build")
 
 @task
